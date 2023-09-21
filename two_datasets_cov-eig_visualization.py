ada6c78ab13412cbd67107a33c9a1177211e23df
Mauro ABIDAL CARRER <mauroabidal@yahoo.fr>
2023-09-22 01:51:31 +0200

    First try at implementing CSP and its visualization, doesn't seem succesfull -_-

--- two_datasets_cov-eig_visualization.py
+++ two_datasets_cov-eig_visualization.py
@@ -20,6 +20,12 @@ def rotated_matrix(matrix, angle):
     return matrix @ np.array([[cos_theta, -sin_theta],
                      [sin_theta, cos_theta]])
 
+def normalize_vector(vector):
+    norm = np.linalg.norm(vector)
+    if norm == 0:
+        return vector  # Avoid division by zero
+    return vector / norm
+
 # def draw_rotated_dataset(data, rotation_angle, dataset_colors, data_index):
 #     #Data generation.
 #     cov_matrix = np.cov(rotated_data, rowvar=False)
@@ -38,41 +44,54 @@ def rotated_matrix(matrix, angle):
 #     ax.legend(by_label.values(), by_label.keys())
 
 def update(_):
-    ax.clear()
+    og_plot.clear()
     rotated_datasets = [rotated_matrix(datasets[i], sliders[i].val) for i in range(len(datasets))]
     for i in range(len(datasets)):
-        ax.scatter(rotated_datasets[i][:, 0], rotated_datasets[i][:, 1], c=datasets_colors[i].scatter, alpha=0.2, label=f'dataset {i}')
+        og_plot.scatter(rotated_datasets[i][:, 0], rotated_datasets[i][:, 1], c=datasets_colors[i].scatter, alpha=0.2, label=f'dataset {i}')
 
     cov_matrices = [np.cov(rotated_data, rowvar=False) for rotated_data in rotated_datasets]
-    def visulize_cov_eig_vecs(cov_matrix):
-        cov_eig_vals, cov_eig_vecs = eig(cov_matrix)
-        for i in range(len(cov_eig_vals)):
-            scaled_eigen_vector = cov_eig_vecs[:, i].real * math.log(cov_eig_vals[i].real) * 2
-            ax.quiver(0, 0, scaled_eigen_vector[0], scaled_eigen_vector[1], angles='xy', scale_units='xy', scale=1, color='red')
-    for cov_matrix in cov_matrices:
-        visulize_cov_eig_vecs(cov_matrix)
+    # def visulize_cov_eig_vecs(cov_matrix):
+    #     cov_eig_vals, cov_eig_vecs = eig(cov_matrix)
+    #     for i in range(len(cov_eig_vals)):
+    #         scaled_eigen_vector = cov_eig_vecs[:, i].real * math.log(cov_eig_vals[i].real) * 2
+    #         og_plot.quiver(0, 0, scaled_eigen_vector[0], scaled_eigen_vector[1], angles='xy', scale_units='xy', scale=1, color='red')
+    # for cov_matrix in cov_matrices:
+    #     visulize_cov_eig_vecs(cov_matrix)
 
     gen_eig_vals, gen_eig_vecs = eig(cov_matrices[0], cov_matrices[1])
     gen_eig_vals = gen_eig_vals.real
     gen_eig_vecs = gen_eig_vecs.real
     for i in range(len(gen_eig_vals)):
         vec = gen_eig_vecs[:, i]
-        scaled_eigen_vector = vec * math.log(gen_eig_vals[i]) * 2
+        # scaled_eigen_vector = vec * math.log(gen_eig_vals[i]) * 2
+        scaled_eigen_vector = vec * 2
         color = matplotlib.colormaps.get_cmap('copper')(gen_eig_vals[i])
-        ax.quiver(0, 0, scaled_eigen_vector[0], scaled_eigen_vector[1], angles='xy', scale_units='xy', scale=1, color=color)
+        og_plot.quiver(0, 0, scaled_eigen_vector[0], scaled_eigen_vector[1], angles='xy', scale_units='xy', scale=1, color=color)
         
     plot_side_length = np.amax([np.linalg.norm(dataset, axis=1) for dataset in datasets]) * 1.1
-    ax.axis([-plot_side_length, plot_side_length, -plot_side_length, plot_side_length])
-    ax.set_aspect(1)
+    og_plot.axis([-plot_side_length, plot_side_length, -plot_side_length, plot_side_length])
+    og_plot.set_aspect(1)
+
+    fil_plot.clear()
+    sort_oreder = np.argsort(gen_eig_vals)[::-1]
+    gen_eig_vecs = np.array([normalize_vector(gen_eig_vec) for gen_eig_vec in gen_eig_vecs]) #[sort_oreder]
+    for i, data in enumerate(rotated_datasets):
+        projected_data = data @ gen_eig_vecs
+        fil_plot.scatter(projected_data[:, 0], projected_data[:, 1], c=datasets_colors[i].scatter, alpha=0.2)
+    fil_plot.axis([-plot_side_length, plot_side_length, -plot_side_length, plot_side_length])
+    fil_plot.set_aspect(1)
+
     plt.draw()
 
 
 
-datasets = [generate_synthetic_data(250, (8, 1.5)),  generate_synthetic_data(250, (9, 2.5))]
+datasets = [generate_synthetic_data(250, (8, 1)),  generate_synthetic_data(250, (9, 1))]
 Dataset_colors = namedtuple('Dataset_colors', 'scatter cov eig')
-datasets_colors = [Dataset_colors('blue', 'green', 'magenta'), Dataset_colors('red', 'pink', 'black')]
+datasets_colors = [Dataset_colors('red', 'green', 'magenta'), Dataset_colors('blue', 'pink', 'black')]
 
-fig, ax = plt.subplots()
+fig, axes = plt.subplots(1, 2)
+og_plot = axes[0]
+fil_plot = axes[1]
 plt.subplots_adjust(bottom=0.25)
 
 # Sliders initialization
