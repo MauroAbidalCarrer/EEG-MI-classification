b1676ab71a44beafb4304d144774facaee726fea
Mauro ABIDAL CARRER <mauroabidal@yahoo.fr>
2023-09-21 15:22:00 +0200

    Added rotation functionality to viusalize its effect on the coavraince and eigenvectors

--- toy_CSP.py
+++ toy_CSP.py
@@ -1,6 +1,7 @@
+import math
 import numpy as np
-from numpy import linalg as la
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 
 def generate_synthetic_data(num_samples, variances):
     cov_matrix = np.diag(variances)  # Create a covariance matrix with the specified variances
@@ -11,6 +12,7 @@ def generate_synthetic_data(num_samples, variances):
     return synthetic_data
 
 def rotation_matrix_2d(theta):
+    theta = math.radians(theta)
     cos_theta = np.cos(theta)
     sin_theta = np.sin(theta)
     
@@ -20,30 +22,45 @@ def rotation_matrix_2d(theta):
     
     return rotation_matrix
 
-data = generate_synthetic_data(1000, (8, 3))
+def update(val):
+    rotation_angle = slider.val
+    rotation_matrix = rotation_matrix_2d(rotation_angle)
+    data = original_data @ rotation_matrix
+    
+    # Calculate the covariance matrix
+    cov_matrix = np.cov(data, rowvar=False)
 
-# Calculate the covariance matrix
-cov_matrix = np.cov(data, rowvar=False)
-print('covariance matrices:')
-print(cov_matrix)
-print(np.dot(data.T, data))
+    # Calculate the eigenvectors and eigenvalues
+    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
 
-# Calculate the eigenvectors and eigenvalues
-eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
-print(eigenvectors)
+    # Clear the previous plot
+    ax.clear()
+    
+    # Plot the data points
+    ax.scatter(data[:, 0], data[:, 1], c='blue', alpha=0.4)
 
-# Plot the data points
-plt.scatter(data[:, 0], data[:, 1], c='blue', alpha=0.4)
+    # Plot the covariance column vectors in green
+    ax.quiver(0, 0, cov_matrix[0, 0], cov_matrix[1, 0], angles='xy', scale_units='xy', scale=1, color='green', label='Covariance Vector 1')
+    ax.quiver(0, 0, cov_matrix[0, 1], cov_matrix[1, 1], angles='xy', scale_units='xy', scale=1, color='green', label='Covariance Vector 2')
 
-# Plot the covariance column vectors in green
-plt.quiver(0, 0, cov_matrix[0, 0], cov_matrix[1, 0], angles='xy', scale_units='xy', scale=1, color='green', label='Covariance Vector 1')
-plt.quiver(0, 0, cov_matrix[0, 1], cov_matrix[1, 1], angles='xy', scale_units='xy', scale=1, color='green', label='Covariance Vector 2')
+    # Plot the eigenvectors as red arrows
+    for i in range(len(eigenvalues)):
+        eigen_vector = eigenvectors[:, i]
+        scaled_eigen_vector = eigenvalues[i] * eigen_vector
+        ax.quiver(0, 0, scaled_eigen_vector[0], scaled_eigen_vector[1], angles='xy', scale_units='xy', scale=1, color='red', label=f'Eigenvector {i+1}')
 
-# Plot the eigenvectors as red arrows
-for i in range(len(eigenvalues)):
-    eigen_vector = eigenvectors[:, i]
-    scaled_eigen_vector = eigenvalues[i] * eigen_vector
-    plt.quiver(0, 0, scaled_eigen_vector[0], scaled_eigen_vector[1], angles='xy', scale_units='xy', scale=1, color='red', label=f'Eigenvector {i+1}')
+    ax.axis('equal')
+    plt.draw()
 
-plt.axis('equal')
+original_data = generate_synthetic_data(1000, (8, 3))
+
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.25)
+
+# Create a slider for the rotation angle
+ax_slider = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+slider = Slider(ax_slider, 'Rotation Angle', 0, 360, valinit=0)
+
+slider.on_changed(update)
+
 plt.show()
