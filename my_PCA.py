671e7b0b67e90d990366f644fc87e7cc0cd4e2a3
Mauro Abidal Carrer <mauroabida@yahoo.fr>
2023-10-09 03:12:50 -0400

    Overall improvments

--- my_PCA.py
+++ my_PCA.py
@@ -1,27 +1,19 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
-from scipy.linalg import eigh, norm, inv, eig
+from scipy.linalg import norm, inv, eig
 from mne.viz import plot_topomap
-import matplotlib.pyplot as plt
 
 
 
-class MyCSP(BaseEstimator, TransformerMixin):
+class MyPCA(BaseEstimator, TransformerMixin):
 
-    def __init__(self, n_components=4):
-        """Set the number of components/features. Can be changed later.
-
-        Args:
-            n_components (int, optional): Number of filters to use when transforming signal. Defaults to 1.
-        """
+    def __init__(self, n_components=3):
         self.n_components = n_components
-        
 
     def fit(self, x, y):
         """
         Args:
             x ndarray, shape(n_epochs, n_channels, n_samples/times): epochs
-            y ndarray, shape(n_epochs): event labels there must be 2 unique values
         Returns:
             self
         """
@@ -29,34 +21,35 @@ class MyCSP(BaseEstimator, TransformerMixin):
         n_unique_labels = len(unique_labels)
         if n_unique_labels != 2: 
             raise ValueError(f"Could not fit CSP, there are {n_unique_labels} unique labels. Two were expected.")
-
-        def mean_cov_of_class(class_index):
+    
+        def PCA_of_class(class_index):
             class_label = unique_labels[class_index]
             class_epochs_mask = np.where(y==class_label)
             class_epochs = x[class_epochs_mask]
-            return np.mean([covarianceMatrix(epoch) for epoch in class_epochs], axis=0)
+            mean_cov = np.mean([covarianceMatrix(epoch) for epoch in class_epochs], axis=0)
+            eig_vals, eig_vecs = eig(mean_cov)
+            eig_vals = eig_vals.real
+            eig_vecs = eig_vecs.real
+            return eig_vals, eig_vecs
 
-        self.filters_ = spatialFilter(mean_cov_of_class(1), mean_cov_of_class(0))
+        eig_vals_0, eig_vecs_0 = PCA_of_class(0)
+        eig_vals_1, eig_vecs_1 = PCA_of_class(1)
+        eig_vals = np.concatenate((eig_vals_0, eig_vals_1))
+        eig_vecs = np.concatenate((eig_vecs_0, eig_vecs_1), axis=1)
+        descending_order = np.argsort(eig_vals)[::-1]
+
+        self.filters_ = eig_vecs.T[descending_order]
         self.patterns_ = pinv(self.filters_)
-        
-        return self
-    
-    def transform(self, x):
-        """
-        Extracts features from signal.
 
-        Args:
-            x (ndarray, shape(n_epochs, n_channels, n_times/samples)): signal epochs
-        Returns:
-            ndarray, shape(n_epochs, n_components, n_times/samples): extracted features
-        """
+        return self
+
+    def transform(self, x):
         tmp_filters = self.filters_[:self.n_components]
         filtered_epochs = np.asarray([tmp_filters @ epoch for epoch in x]) #(n_epochs, n_features, n_samples)
         final_output = (filtered_epochs**2).mean(axis=2) #(n_epochs, n_features)
         final_output = np.log(final_output) #Removing the log seems to increase classification accuracy.
         return final_output
     
-
     def plot_patterns(self, raw_info, ch_type='eeg'):
         for component in self.patterns_.T[:self.n_components]:
             plot_topomap(
@@ -66,32 +59,10 @@ class MyCSP(BaseEstimator, TransformerMixin):
                 ch_type=ch_type
             )
     
-
 # covarianceMatrix takes a matrix A and returns the covariance matrix, scaled by the variance
 def covarianceMatrix(A):
     cov_mat = A @ A.T
     return cov_mat / np.trace(cov_mat) 
-
-def spatialFilter(Ra,Rb):
-    def sorted_eig(vals, vecs):
-        ord = np.argsort(vals)[::-1]
-        return vals[ord].real, vecs[:, ord].real
-
-    # Compute PCA whitening matrix.
-    E, U = sorted_eig(*eig(Ra + Rb))
-    white_mat = np.sqrt(inv(np.diag(E))) @ U.T
-    white_mat = white_mat.real
-
-    # The mean covariance matrices may now be transformed.
-    Sa = white_mat @ Ra @ white_mat.T
-    Sb = white_mat @ Rb @ white_mat.T
-
-    # Find and sort the generalized eigenvalues and eigenvector
-    _, U1 = sorted_eig(*eig(Sa, Sb))
-
-    # The projection matrix (the spatial filter) may now be obtained
-    SFa = U1.T @ white_mat
-    return SFa.astype(np.float32)
 
 def pinv(a, rtol=None):
     """Compute a pseudo-inverse of a matrix."""
